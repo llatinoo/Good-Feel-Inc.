175772b672c2ac6bcf34ae3d538036b7e2c773da
tobiaswarth <warthtobias@yahoo.de>
2015-12-14 12:09:48 +0100

    ScreenManager und Animation

---
+++ RPG/Animations/Animations.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+namespace SchulProjekt.Animation
+{
+    class Animations
+    {
+        //Textur der Animation
+        Texture2D sprite;
+
+        //Anzeigegröße des sprites
+        float displayedScale;
+
+        //Zeit des letzten Updates
+        int elapsedTime;
+
+        //Zeit die ein Frame angezeigt bis zum nächsten
+        int frameDisplayTime;
+
+        //Die anzahl an frames des spritesheets
+        int countSpritesheetFrames;
+
+        //Index des aktuell angezeigten Frames
+        int currentFrame;
+
+        //Farbe des Frames
+        Color color;
+
+        //Teil des Spritesheets der angezeigt werden soll
+        Rectangle sourceRect = new Rectangle();
+
+        //Wo das Frame auf dem Bildschirm angezeigt werden soll
+        Rectangle destinationRect = new Rectangle();
+
+        //Breite des Frames
+        public int frameWidth;
+
+        //Höhe des Frames
+        public int frameHeight;
+
+        //Status der Animation
+        public bool active;
+
+        //gibt an ob die animation weiter abspielt oder stoppt
+        public bool loop;
+
+        //Breite des gegebenen Frames
+        public Vector2 position;
+
+        public void Initialize(Texture2D sprite, Vector2 position, int frameWidth, int frameHeight, 
+            int countSpritesheetFrames, int frameDisplayTime, Color color, float displayedScale, bool loop)
+        {
+            this.sprite = sprite;
+            this.position = position;
+            this.frameWidth = frameWidth;
+            this.frameHeight = frameHeight;
+            this.countSpritesheetFrames = countSpritesheetFrames;
+            this.frameDisplayTime = frameDisplayTime;
+            this.color = color;
+            this.displayedScale = displayedScale;
+            this.loop = loop;
+
+            //Zeit wird auf 0 gesetzt
+            elapsedTime = 0;
+            currentFrame = 0;
+
+            //standartmäßig Animation aktiv setzen
+            active = true;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            if (active == false)
+            {
+                return;
+            }
+            //vergangene Zeit updaten
+            elapsedTime += (int)gameTime.ElapsedGameTime.TotalMilliseconds;
+
+            //Wenn vergangene Zeit größer als anzeigezeit ist, wird das aktuelle Frame geändert
+            if (elapsedTime > frameDisplayTime)
+            {
+                currentFrame++;
+
+                if(currentFrame == countSpritesheetFrames)
+                {
+                    currentFrame = 0;
+
+                    //Wenn die Animation nicht wiederholt werden soll wird sie deaktiviert
+                    if(!loop)
+                    {
+                        active = false;
+                    }
+                }
+
+                //vergangene Zeit wird zurückgesetzt
+                elapsedTime = 0;
+            }
+
+            //Der korrekte Frame wird gewählt
+            sourceRect = new Rectangle(frameWidth * currentFrame, 0, frameWidth, frameHeight);
+
+            //Ziel des anzuzeigenden Frames
+            destinationRect = new Rectangle((int)position.X - (int)(frameWidth * displayedScale) / 2, 
+            (int)position.Y - (int)(frameHeight * displayedScale) / 2, (int)(frameWidth * displayedScale), (int)(frameHeight * displayedScale));
+                
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            //nur wenn die Animation aktiv ist wird sie dargestellt
+            if(active)
+            {
+                spriteBatch.Draw(sprite,destinationRect,sourceRect,color);
+            }
+        }
+    }
+}
---
+++ RPG/ScreenManager/Screens.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace SchulProjekt
+{
+    class Screens
+    {
+        //Tastatur Status
+        KeyboardState previousKeyboardState;
+        KeyboardState currentKeyboardState;
+        //SpielStatus
+        enum GameState {mainMenu, options, storyScreen, battleScreen}
+        GameState gameState;
+
+        //Liste der GUI Elemente
+        List<GUIElement> mainMenu = new List<GUIElement>();
+        List<GUIElement> options = new List<GUIElement>();
+        List<GUIElement> storyScreen = new List<GUIElement>();
+        List<GUIElement> battleScreen = new List<GUIElement>();
+
+        public Screens()
+        {
+            mainMenu.Insert(0, new GUIElement("MainMenuScreenbackground",0,0));
+            mainMenu.Insert(1, new GUIElement("MainMenuPlayButton", 150, 150));
+
+            options.Insert(0, new GUIElement("OptionsScreenBackground", 0, 0));
+            options.Insert(1, new GUIElement("OptionsExitButton", 150, 150));
+            options.Insert(2, new GUIElement("OptionsContinueButton", 200, 100));
+
+            storyScreen.Insert(0, new GUIElement("StoryScreenBackground", 0, 0));
+
+            /*
+            storyScreen.Insert(0, new GUIElement("StoryScreenBackground", 0, 0));
+            storyScreen.Insert(1, new GUIElement("StoryTextBackground",0,0));
+            //CharacterPortrait
+            storyScreen.Insert(2, new GUIElement("StandartCharacterPortrait", 0, 0));
+            
+            mainMenu.Insert(0, new GUIElement("MainMenuScreenBackground", 0, 0));
+            mainMenu.Insert(1, new GUIElement("MainMenubackgroundButton", 0, 0));
+            mainMenu.Insert(2, new GUIElement("MainMenuNewGameButton", 0, 0));
+            mainMenu.Insert(3, new GUIElement("MainMenuLoadGameButton", 0, 0));
+            mainMenu.Insert(4, new GUIElement("MainMenuQuitButton", 0, 0));
+
+            options.Insert(0, new GUIElement("OptionsScreenBackground", 0, 0));
+            options.Insert(1, new GUIElement("OptionsMenuBackground", 0, 0));
+            options.Insert(2, new GUIElement("OptionsContinueButton", 0, 0));
+            options.Insert(3, new GUIElement("OptionsSaveButton", 0, 0));
+            options.Insert(4, new GUIElement("OptionsQuitButton", 0, 0));
+            */
+
+        }
+
+        public void LoadContent(ContentManager content)
+        {
+            foreach(GUIElement element in mainMenu)
+            {
+                element.LoadContent(content);
+                element.clickEvent += OnClick;
+            }
+            //verschiebt das GUI Element mit einem bestimmten Namen 
+            //main.Find(x => x.AssetName == "Play").moveElement(0,-100);
+            foreach(GUIElement element in options)
+            {
+                element.LoadContent(content);
+                element.clickEvent += OnClick;
+            }
+
+            foreach (GUIElement element in storyScreen)
+            {
+                element.LoadContent(content);
+                element.clickEvent += OnClick;
+            }
+
+            foreach (GUIElement element in battleScreen)
+            {
+                element.LoadContent(content);
+                element.clickEvent += OnClick;
+            }
+            
+        }
+        
+        public void Update()
+        {
+            //Tastatur Status wird gespeichert
+            previousKeyboardState = currentKeyboardState;
+            //aktueller Tastatur Status wird gelesen
+            currentKeyboardState = Keyboard.GetState();
+
+            if(currentKeyboardState.IsKeyDown(Keys.Escape) && gameState != GameState.mainMenu)
+            {
+                gameState = GameState.options;
+            }
+            
+            switch (gameState)
+            {
+                case GameState.mainMenu:
+                    foreach (GUIElement element in mainMenu)
+                    {
+                        element.Update();
+                    }
+                    break;
+                case GameState.options:
+                    foreach (GUIElement element in options)
+                    {
+                        element.Update();
+                    }
+                    break;
+                case GameState.storyScreen:
+                    foreach (GUIElement element in storyScreen)
+                    {
+                        element.Update();
+                    }
+                    break;
+                case GameState.battleScreen:
+                    foreach (GUIElement element in storyScreen)
+                    {
+                        element.Update();
+                    }
+                    break;
+            }
+            
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+            switch (gameState)
+            {
+                case GameState.mainMenu:
+                    foreach (GUIElement element in mainMenu)
+                    {
+                        element.Draw(spriteBatch);
+                    }
+                    break;
+                case GameState.options:
+                    foreach (GUIElement element in options)
+                    {
+                        element.Draw(spriteBatch);
+                    }
+                    break;
+                case GameState.storyScreen:
+                    foreach (GUIElement element in storyScreen)
+                    {
+                        element.Draw(spriteBatch);
+                    }
+                    break;
+                case GameState.battleScreen:
+                    foreach (GUIElement element in storyScreen)
+                    {
+                        element.Draw(spriteBatch);
+                    }
+                    break;
+            }
+           
+        }
+
+        //ändert ein beliebiges GUI Element 
+        public void ChangeGUIElement(List<GUIElement> screen, int index, GUIElement newTexture)
+        {
+            screen.RemoveAt(index);
+            screen.Insert(index,newTexture);
+        }
+
+        public void OnClick(string element)
+        {
+            if(element == "MainMenuPlayButton")
+            {
+                gameState = GameState.storyScreen;
+                
+            }
+            if(element == "OptionsContinueButton")
+            {
+                gameState = GameState.storyScreen;
+            }
+            if(element == "OptionsExitButton")
+            {
+                //Quit Game
+            }
+        }
+    }
+}
