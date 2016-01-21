40814c29cb4424be8e481cefa113097964487b65
Lucas Dengler <l.dengler@faz.de>
2016-01-21 09:00:16 +0100

    Game breaking bug behoben Sound abspielbar gemacht

--- RPG/Sounds/Sound.cs
+++ RPG/Sounds/Sound.cs
@@ -11,7 +11,7 @@ namespace RPG
         System.Media.SoundPlayer test = new System.Media.SoundPlayer();
         public void LoadContent(ContentManager content)
         {
-            test.SoundLocation = "C:\\Users\\dengler\\Documents\\GitHubVisualStudio\\RPG-Game\\RPG\\Content\\Sounds\\Life_converted.wav";
+            test.SoundLocation = "Content\\Sounds\\Life_converted.wav";
             test.Load();
             test.Play();
         }
