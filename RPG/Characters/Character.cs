636380a15a50cbccc1efc830a334f47f4f5c7a9b
Lucas Dengler <l.dengler@faz.de>
2016-01-21 08:08:23 +0100

    wARUM TUST DU DAS

--- RPG/Characters/Character.cs
+++ RPG/Characters/Character.cs
@@ -3,7 +3,6 @@ using Microsoft.Xna.Framework.Graphics;
 using System.Collections.Generic;
 using RPG.Skills;
 using RPG.Skills.StatusEffects;
-using SchulProjekt.Animation;
 
 namespace RPG.Characters
 {
@@ -30,7 +29,7 @@ namespace RPG.Characters
     public class Character
     {
         //Grafikdaten des Charakters
-        public Animations Sprite { get; private set; }
+        public Animation Sprite { get; private set; }
 
         //Name
         public string Name { get; private set; }
@@ -73,10 +72,6 @@ namespace RPG.Characters
             get { return this.FightLuck; }
             set { this.FightLuck = MathHelper.Clamp(this.FightLuck, 0, 70); }
         }
-
-        //Ausrüstung
-        public Weapon RightWeapon { get; set; }
-        public Weapon LefWeapon { get; set; }
 
         //Level Attribute
         public int Level { get; set; }
@@ -122,12 +117,6 @@ namespace RPG.Characters
         public void RemoveSkill(Skill removeSkill)
         {
             this.Skills.Remove(removeSkill);
-        }
-
-        public void InitzializeSprite(Texture2D texture, Vector2 position, int frameWidth, int frameHeight,
-            int countSpritesheetFrames, int frameDisplayTime, Color color, float displayedScale, bool loop)
-        {
-            this.Sprite.Initialize(texture, position, frameWidth, frameHeight, countSpritesheetFrames, frameDisplayTime, color, displayedScale, loop);
         }
 
         public void UpdateStat(int updateAmmount, MainAttributes mainAttributeToUpdate)
